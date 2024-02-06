1fa65649b3f9a47ae81a9e4fa2e017e327e402a7
John <88872618+7Johnsz@users.noreply.github.com>
2024-02-06 18:50:51 -0300

    add extra

--- src/schemas/models.py
+++ src/schemas/models.py
@@ -1,18 +1,9 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-
-from typing import Optional
 from pydantic import BaseModel
 
 # Schemas
 
 class Password(BaseModel):
     length: int
-
-    def length(self, length):
-        self.length = length
-
-        return self.length
-
-password = Password()
     
+    class Config:
+        extra = "forbid"
